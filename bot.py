64513f19b2ef6ea33df3f5f6839d2886b0b65ac4
Lando <landonlewisturner@gmail.com>
2023-05-06 16:37:00 -0400

    chaos wip

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 import random
 
-import asyncio
 import discord
 import spotipy
 import spotipy.util as util
@@ -10,8 +9,7 @@ import wavelink
 
 from discord.ext import commands
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
-
+from wavelink.ext import spotify
 
 load_dotenv()
 
@@ -19,7 +17,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI') #TODO: env me
+redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI') 
 spotify_scope = 'user-library-read playlist-read-private user-modify-playback-state'
 WAVELINK_URI = os.getenv('WAVELINK_URI')
 WAVELINK_PASSWORD = os.getenv('WAVELINK_PASSWORD')
@@ -33,57 +31,79 @@ token = util.prompt_for_user_token(
     redirect_uri=redirect_uri
     )
 
+class CustomPlayer(wavelink.Player):
+    def __init__(self):
+        super().__init__()
+        self.queue = wavelink.Queue()
+        # TODO: use this to implement after poc for other users spotify premium
+
+class BardBot(commands.Bot):
+    def __init__(self) -> None:
+        intents = discord.Intents.all()
+        super().__init__(intents=intents, command_prefix='!')
+
+    async def on_ready(self) -> None:
+        print(f'Logged in {self.user} | {self.user.id}')
+        print('They are traveling in the following realms:')
+        for guild in bot.guilds:
+            print(f'{guild}(id: {guild.id})')
+            members = '\n - '.join([member.name for member in guild.members])
+            print(f'Guild Members:\n - {members}')
+        print('Ready to rock!')
+
+    async def setup_hook(self) -> None:
+        """Connects bot to the wavelink server"""
+        print('Starting Node connect')
+        try:
+            node: wavelink.Node = wavelink.Node(
+                uri=WAVELINK_URI,
+                password=WAVELINK_PASSWORD,
+            )
+            await wavelink.NodePool.connect(client=bot, nodes=[node], spotify=sp_wavelink)
+            print(sp_wavelink)
+
+        except Exception as e:
+            print(f'Connection failed due to: {e}')
+            pass
 # Clients
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-# sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
-# )
+# bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = BardBot()
 sp = spotipy.Spotify(auth=token)
+sp_wavelink = spotify.SpotifyClient(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
 
-async def node_connect():
-    """Connects bot to the wavelink server"""
-    print('Starting Node connect')
-    await bot.wait_until_ready()
-    try:
-        node: wavelink.Node = wavelink.Node(
-            uri=WAVELINK_URI,
-            password=WAVELINK_PASSWORD,
-        )
-        await wavelink.NodePool.connect(client=bot, nodes=[node], spotify=sp)
-    except Exception as e:
-        print(f'Connection failed due to: {e}')
-        pass
+# async def node_connect():
+#     """Connects bot to the wavelink server"""
+#     print('Starting Node connect')
+#     await bot.wait_until_ready()
+#     try:
+#         node: wavelink.Node = wavelink.Node(
+#             uri=WAVELINK_URI,
+#             password=WAVELINK_PASSWORD,
+#         )
+#         await wavelink.NodePool.connect(client=bot, nodes=[node], spotify=sp_wavelink)
+#     except Exception as e:
+#         print(f'Connection failed due to: {e}')
+#         pass
 
-@bot.event
-async def on_wavelink_node_ready(node: wavelink.Node):
-    print(f'Node {node.id} is ready')
+# @bot.event
+# async def on_wavelink_node_ready(node: wavelink.Node):
+#     print(f'Node {node.id} is ready')
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
-    print('They are traveling in the following realms:')
-    for guild in bot.guilds:
-        print(f'{guild}(id: {guild.id})')
-        members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
-    print('Connecting to Wavelink server...')
-    # TODO: when making this past POC, have it check if the user has configured spotify and prompt to if not
-    try:
-        bot.loop.create_task(node_connect())
-    except Exception as e:
-        print(f'Failed to connect to Wavelink server: {e}')
 
-# plays a video from youtube, intended for tutorial purposes, remove me if in repo
-@bot.command()
-async def yt(ctx: commands.Context):
-    # if not ctx.voice_client:
-    #     vc:wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-    # else:
-    #     vc: wavelink.Player = ctx.voice_client
-    # join the voice chat if not already there
-    vc: wavelink.Player = ctx.voice_client if ctx.voice_client else await ctx.author.voice.channel.connect(cls=wavelink.Player)
-    
-    await vc.play('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-
+# @bot.event
+# async def on_ready():
+#     print(f'{bot.user} has connected to Discord!')
+#     print('They are traveling in the following realms:')
+#     for guild in bot.guilds:
+#         print(f'{guild}(id: {guild.id})')
+#         members = '\n - '.join([member.name for member in guild.members])
+#         print(f'Guild Members:\n - {members}')
+#     print('Connecting to Wavelink server...')
+#     # TODO: when making this past POC, have it check if the user has configured spotify and prompt to if not
+#     try:
+#         bot.loop.create_task(node_connect())
+#     except Exception as e:
+#         print(f'Failed to connect to Wavelink server: {e}')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -113,8 +133,38 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     await ctx.send(', '.join(dice))
 
 @bot.command(name='play')
-async def play(ctx, query: str):
+async def play(ctx: commands.context, query: str):
+    from wavelink.ext import spotify
+
+    if not getattr(ctx.author.voice, "channel", None):
+        ctx.send('Sorry, I can only play in voice channels!')
+    custom_player = CustomPlayer()
     vc: wavelink.Player = ctx.voice_client if ctx.voice_client else await ctx.author.voice.channel.connect(cls=wavelink.Player)
+
+
+    if vc.queue.is_empty and not vc.is_playing():
+        try:
+            results = sp.search(q='bardic-inspiration:combat', type='playlist')
+            playlist = next((p for p in results['playlists']['items'] if p['owner']['display_name'] == 'Landon Turner'))
+            tracks = sp.playlist_tracks(playlist_id=playlist.get('id'), fields='items',additional_types=('track',))
+            print(tracks['items'][0]['track']['external_urls']['spotify'])
+            track_url = tracks['items'][0]['track']['external_urls']['spotify']
+            decoded = spotify.decode_url(track_url)
+            print(decoded)
+
+            vc.autoplay = True
+            print('kosher')
+            track = await spotify.SpotifyTrack.search(track_url)
+
+            print('evry ting ire')
+            await vc.play(track, populate=True)
+            print('everyting still ire')
+            await ctx.send(f'Playing `{tracks.title}`')
+        except Exception as e:
+            print(e)
+            return await ctx.send("oops")
+    else:
+        await vc.queue.put_wait(track)
 
 
 bot.run(TOKEN)
