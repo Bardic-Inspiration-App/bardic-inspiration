8c05ba78d0e719d308918cfc5c66cb55c1b6e0f6
Landon Turner <landonlewisturner@gmail.com>
2023-05-26 12:48:58 -0400

    cleanup, add test bot token

--- bot.py
+++ bot.py
@@ -14,14 +14,14 @@ from utils.util import shuffle_list, roll_dice
 load_dotenv()
 
 # Global Variables
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN', 'MTExMTY5MDE0Nzg1MTkyNzcwMg.GmFjcU.-4iDrvDMLfKa-tiRc1SmBvoOT2NwpWP-iPFDGQ')
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI') 
 spotify_scope = 'user-library-read playlist-read-private user-modify-playback-state'
 WAVELINK_URI = os.getenv('WAVELINK_URI')
 WAVELINK_PASSWORD = os.getenv('WAVELINK_PASSWORD')
-# TODO: make this dynamic so that it still functions but doesn't need spotify login <- I think I will require spotify premium to protect my aims here
+
 username = os.getenv('SPOTIFY_USERNAME')
 token = util.prompt_for_user_token(
     username=username, 
@@ -38,7 +38,7 @@ class CustomPlayer(wavelink.Player):
     def __init__(self):
         super().__init__()
         self.queue = wavelink.Queue()
-        # TODO: use this to implement after poc for other users spotify premium
+        # TODO: use this to implement after poc for other users 
 
 class BardBot(commands.Bot):
     def __init__(self) -> None:
@@ -77,19 +77,10 @@ async def on_command_error(ctx, error):
         if hasattr(error.original, 'reason') and error.original.reason == 'PREMIUM_REQUIRED':
             await ctx.send('You do not have a premium Spotify account configured to run this command.\nRun: `!configure spotify`.')
 
-@bot.command(name='configure')
-async def configure(ctx, aspect: str):
-    # TODO: use python switch since we have 3.10
-    if aspect.lower().strip() == 'spotify':
-        #FIXME: hard authing to POC but authing should be handled here
-        pass
-
-# TODO: turn this into a D&D specific roll function
-## should take number of dice then the dX die string (use switch or something else)
 @bot.command(name='roll')
 async def roll(ctx, dice_string: str):
     number, sides = dice_string.split('d')
-    result = roll_dice(number, sides)
+    result = roll_dice(int(number), int(sides))
     await ctx.send(result)
 
 @bot.command(name='play')
@@ -103,12 +94,11 @@ async def play(ctx: commands.Context, query: str):
         # set a standard that plays at a backgroung level. default volume is aggressive
         await vc.set_volume(5)
 
-        # TODO: if play is run again and it is playing have it pause and switch to the other playlist
+        # if play is run again and it is playing have it stop and reset
         await vc.stop()
         vc.queue = wavelink.Queue()
-        # command always starts with a clean playlist queue
-        # vc.queue = wavelink.Queue()
-        # TODO: see if we will need to pause/stop as well so that the most recent play command always overrides the last 
+
+        # TODO: create a function that returns a playlist from this switch statement
         match query:
             case 'combat':
                 search = 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp0At0z5aiNmjyBoL9Vvj_G1'
@@ -134,47 +124,5 @@ async def play(ctx: commands.Context, query: str):
         print('damn', e)
         print(traceback.print_exc())
         return await ctx.send("Apologies, something unforseen has gone wrong.")
-
-
-# The below is not implemented and proven difficult to work with in current versions, hopeful to be revisited
-@bot.command(name='splay')
-async def splay(ctx: commands.Context, query: str):
-    """Play type of playlist from Spotify"""
-    if not getattr(ctx.author.voice, "channel", None):
-        ctx.send('Sorry, I can only play in voice channels!')
-    vc: wavelink.Player = ctx.voice_client if ctx.voice_client else await ctx.author.voice.channel.connect(cls=wavelink.Player)
-    results = sp.search(q='bardic-inspiration:combat', type='playlist')
-    playlist = next((p for p in results['playlists']['items'] if p['owner']['display_name'] == 'Landon Turner'))
-    tracks = sp.playlist_tracks(playlist_id=playlist.get('id'), fields='items',additional_types=('track',))
-    # print(tracks['items'][0]['track']['external_urls']['spotify'])
-    track_url = tracks['items'][0]['track']['external_urls']['spotify']
-    decoded = spotify.decode_url(track_url)
-    if not decoded or decoded['type'] is not spotify.SpotifySearchType.track:
-        await ctx.send('Sorry, I had an issue finding the lyrics to a song.')
-        return
-    try:
-        vc.autoplay = True
-        track = find_track(track_url)
-        # track = await spotify.SpotifyTrack.search(track_url)
-        if vc.queue.is_empty and not vc.is_playing():
-            print('evry ting ire')
-            await vc.play(track, populate=True)
-            print('everyting still ire')
-            await ctx.send(f'Playing `{tracks.title}`')
-        else:
-            await vc.queue.put_wait(track)
-    except Exception as e:
-        print('damn', e)
-        print(traceback.print_exc())
-       
-        return await ctx.send("oops")
-
-# @tasks.loop(seconds=5)
-async def find_track(query):
-    """Finds the track using the wavelink Spotify extensions. Has to run in it's own async function."""
-    track = await spotify.SpotifyTrack.search(query)
-    print('track', track)
-    return track
-
 
 bot.run(TOKEN)
---
+++ spotify/spotify.py
@@ -0,0 +1,39 @@
+# The below is not implemented and proven difficult to work with in current versions, hopeful to be revisited
+# @bot.command(name='splay')
+# async def splay(ctx: commands.Context, query: str):
+#     """Play type of playlist from Spotify"""
+#     if not getattr(ctx.author.voice, "channel", None):
+#         ctx.send('Sorry, I can only play in voice channels!')
+#     vc: wavelink.Player = ctx.voice_client if ctx.voice_client else await ctx.author.voice.channel.connect(cls=wavelink.Player)
+#     results = sp.search(q='bardic-inspiration:combat', type='playlist')
+#     playlist = next((p for p in results['playlists']['items'] if p['owner']['display_name'] == 'Landon Turner'))
+#     tracks = sp.playlist_tracks(playlist_id=playlist.get('id'), fields='items',additional_types=('track',))
+#     # print(tracks['items'][0]['track']['external_urls']['spotify'])
+#     track_url = tracks['items'][0]['track']['external_urls']['spotify']
+#     decoded = spotify.decode_url(track_url)
+#     if not decoded or decoded['type'] is not spotify.SpotifySearchType.track:
+#         await ctx.send('Sorry, I had an issue finding the lyrics to a song.')
+#         return
+#     try:
+#         vc.autoplay = True
+#         track = find_track(track_url)
+#         # track = await spotify.SpotifyTrack.search(track_url)
+#         if vc.queue.is_empty and not vc.is_playing():
+#             print('evry ting ire')
+#             await vc.play(track, populate=True)
+#             print('everyting still ire')
+#             await ctx.send(f'Playing `{tracks.title}`')
+#         else:
+#             await vc.queue.put_wait(track)
+#     except Exception as e:
+#         print('damn', e)
+#         print(traceback.print_exc())
+       
+#         return await ctx.send("oops")
+
+# # @tasks.loop(seconds=5)
+# async def find_track(query):
+#     """Finds the track using the wavelink Spotify extensions. Has to run in it's own async function."""
+#     track = await spotify.SpotifyTrack.search(query)
+#     print('track', track)
+#     return track
