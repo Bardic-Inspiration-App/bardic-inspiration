1befe35bf711fb421684f34bb4fb0d648152bcc8
Landon Turner <landonlewisturner@gmail.com>
2023-05-26 12:50:18 -0400

    discord didnt like me pushing my bot token which is good lmao my bad

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from utils.util import shuffle_list, roll_dice
 load_dotenv()
 
 # Global Variables
-TOKEN = os.getenv('DISCORD_TOKEN', 'MTExMTY5MDE0Nzg1MTkyNzcwMg.GmFjcU.-4iDrvDMLfKa-tiRc1SmBvoOT2NwpWP-iPFDGQ')
+TOKEN = os.getenv('DISCORD_TOKEN')
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI') 
