1e8527a3c3b7ec58e032a1ed357d2355bca45ac3
Lando <landonlewisturner@gmail.com>
2023-06-12 19:32:13 -0400

    issue-2: add rest of playlist and volume adjust

--- utils/util.py
+++ utils/util.py
@@ -1,13 +1,13 @@
 import random
 
-def shuffle_list(items: list):
+def shuffle_list(items: list) -> list:
     n = len(items)
     for i in range(n -1, 0, -1):
         j = random.randint(0, i)
         items[i], items[j] = items[j], items[i]
     return items
 
-def roll_dice(number: int, sides: int):
+def roll_dice(number: int, sides: int) -> str:
     """
     Rolls set of common die based on args
     params:
@@ -20,7 +20,7 @@ def roll_dice(number: int, sides: int):
         for _ in range(number)
     ])
 
-def get_playlist_url(query: str):
+def get_playlist_url(query: str) -> str:
     match query:
         case 'combat':
             return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp0At0z5aiNmjyBoL9Vvj_G1'
@@ -28,5 +28,21 @@ def get_playlist_url(query: str):
             return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp2VI5evDa_Lpqff5hwL7vg5'
         case 'explore':
             return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp10P0s0EkmWzFenouJe-04b'
+        case 'seas':
+            return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp2uW9l7DFC6dZNNvw9OGzG2'
+        case 'city':
+            return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp0DX8_Ki3O-8xKNycAL0tRC'
+        case 'tavern':
+            return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp21M7xsacZmVxptF_Wv0s3y'
+        case 'infernal':
+            return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp2WDTlRv5oADdO_dP9DSJPD'
+        case 'forest':
+            return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp1VdBX3o5iKpk_8RZ3FWjXN'
+        case 'jungle':
+            return 'https://www.youtube.com/playlist?list=PLMK9kbhhnbp2ULvcO1F2UNAg9kkQSH-rt'
         case _:
-            return None
+            return ''
+
+def return_play_commands() -> str:
+    playlists = ['combat', 'tense', 'explore', 'seas', 'city', 'tavern', 'infernal', 'forest', 'jungle']
+    return " ".join([f"\n- `{command}`" for command in playlists])
